1003e6a7fe2bf599f0a933ea7f19b5fda8632cb7
= <=>
2024-10-18 13:09:46 +0600

    Test 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
 "use client";
 
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [user, setUser] = useState();
   useEffect(() => {
     axios
       .get(
         "https://api.telegram.org/bot7863360864:AAH6qjVchEI_IhEAqTWC3KqXd5ZQOVDoTpY/getUpdates"
       )
-      .then((res) => console.log(res))
+      .then((res) => setUser(res.data.result.update_id))
       .catch((err) => console.log(err));
   }, []);
-  return <div>Home</div>;
+  return <div> hello {user} </div>;
 };
 
 export default Home;
